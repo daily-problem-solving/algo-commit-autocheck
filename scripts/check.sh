1aba68ae6987aa885bfef207007d649614156402
iljun yun <godhkekf244@gmail.com>
2024-01-27 19:58:01 +0900

    feat: GitHub Secret 변수 올바르게 가져오는지 확인
    Signed-off-by: iljun yun <godhkekf244@gmail.com>

---
+++ scripts/check.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+count=$(cat users.json | jq '. | length')
+
+for((i = 0; i < $count; i++)); do
+    curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${{secrets.TOKEN_GITHUB}}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/daily-problem-solving/252/commits
+done
